ade50629db7907fd222b1c14f046b0e676d48c18
Sourav Debnath <132735088+Sourav2507@users.noreply.github.com>
2025-07-26 12:54:15 +0530

    Update config.py

--- backend/config/config.py
+++ backend/config/config.py
@@ -22,7 +22,7 @@ class LocalDevelopmentConfig(Config):
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = 'developersouravdebnath@gmail.com'
-    MAIL_PASSWORD = 'gskdzfzeithjyqfc'
+    MAIL_PASSWORD = 'Secret_not_to_be_shared'
     MAIL_DEFAULT_SENDER = 'developersouravdebnath@gmail.com'
 
     CELERY = dict(
